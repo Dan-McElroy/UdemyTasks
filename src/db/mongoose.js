a8ddf7ac8addb74cfe665ac39976057c7cc5dc9b
Dan McElroy <daniel@mcelroys.net>
2021-01-28 14:24:59 +0100

    Added some validation and sanitation to db models.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -8,23 +9,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error ('Email is invalid')
+            }
+        },
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value <0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        required: true
     }
 })
 
-const firstTask = new Task({
-    description: 'First task',
-    completed: true
-}).save().then(console.log).catch(console.log)
+const me = new User({name: '     Steve    ', email: 'DJTARIQ@PalACE.COM'}).save().then(console.log).catch(console.log)
