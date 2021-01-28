a193afe4955fc169137cf2f1312ac81361547ad9
Dan McElroy <daniel@mcelroys.net>
2021-01-28 17:27:39 +0100

    Reordered API endpoints.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,16 @@ app.post('/users', async (req, res) => {
     
 })
 
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
+
 app.get('/users/:id', async (req, res) => {
 
     const _id = req.params.id
@@ -36,16 +46,6 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        console.log(e)
-        res.sendStatus(500)
-    }
-})
-
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     
@@ -54,6 +54,16 @@ app.post('/tasks', async (req, res) => {
         res.status(201).send(task)
     } catch(e) {
         res.status(400).send(e)
+    }
+})
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch(e) {
+        console.log(e)
+        res.sendStatus(500)
     }
 })
 
@@ -66,16 +76,6 @@ app.get('/tasks/:id', async (req, res) => {
             return res.sendStatus(404)
         }
         res.send(task)
-    } catch(e) {
-        console.log(e)
-        res.sendStatus(500)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
     } catch(e) {
         console.log(e)
         res.sendStatus(500)
