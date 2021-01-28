c32dfc538ba563c9cba9c786c0321754e5540d94
Dan McElroy <daniel@mcelroys.net>
2021-01-28 12:35:12 +0100

    Exploring object IDs

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,10 @@
 const mongodb = require('mongodb')
 
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = mongodb
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -11,15 +15,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Dan',
-    //     age: 28
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Vikram',
+        age: 32
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+        console.log(result.ops)
+    })
 
     // db.collection('users').insertMany([
     //     {
@@ -35,23 +40,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //     console.log(result.ops)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Do the first thing',
-            completed: true
-        },
-        {
-            description: 'Do the second thing',
-            completed: false
-        },
-        {
-            description: 'Do the third thing',
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable insert tasks!')
-        }
-        console.log(result.ops)
-    })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Do the first thing',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Do the second thing',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Do the third thing',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable insert tasks!')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+
 })
