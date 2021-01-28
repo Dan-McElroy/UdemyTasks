13b87d2f09f0dc00a02489e3aeb624b1cfd8c74d
Dan McElroy <daniel@mcelroys.net>
2021-01-28 12:16:24 +0100

    Tested inserting documents.

--- mongodb.js
+++ mongodb.js
@@ -11,8 +11,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Dan',
-        age: 28
+    // db.collection('users').insertOne({
+    //     name: 'Dan',
+    //     age: 28
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //     name: 'Jen',
+    //     age: 31
+    // }, {
+    //     name: 'Gunther',
+    //     age: 65
+    // }], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Do the first thing',
+            completed: true
+        },
+        {
+            description: 'Do the second thing',
+            completed: false
+        },
+        {
+            description: 'Do the third thing',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable insert tasks!')
+        }
+        console.log(result.ops)
     })
 })
