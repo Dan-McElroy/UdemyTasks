91dcfa9d4519fd568e7ddd3f5512d14363629b78
Dan McElroy <daniel@mcelroys.net>
2021-01-29 18:56:25 +0100

    Added sorting to tasks endpoint.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,9 +5,7 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'thisishisnewcourse')
-        console.log(decoded._id)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-        console.log('User:',decoded)
         if (!user) {
             throw new Error()
         }
