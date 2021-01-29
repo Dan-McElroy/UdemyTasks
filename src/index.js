1f1f91cfb6562de32f69cce5d5ecbe46228e2ce0
Dan McElroy <daniel@mcelroys.net>
2021-01-29 16:10:41 +0100

    Authenticated task endpoints

--- src/index.js
+++ src/index.js
@@ -14,19 +14,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async() => {
-    // const task = await Task.findById('60141e79126fec43e0b92f0f')
-
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('60141e14a1a6d60fb40e525a')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,21 +17,22 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         console.log(e)
         res.sendStatus(500)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
+        
         if (!task) {
             return res.sendStatus(404)
         }
@@ -42,7 +43,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const disallowedUpdates = updates.filter(update => !allowedUpdates.includes(update))
@@ -50,15 +51,17 @@ router.patch('/tasks/:id', async (req, res) => {
     if (disallowedUpdates.length > 0) {
         return res.status(400).send({ error: `Invalid updates! Field(s) ${disallowedUpdates.join(', ')} cannot be updated.` })
     }
+
     try {
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
-        updates.forEach(update => task[update] = req.body[update])
-
-        await task.save()
         if (!task) {
             return res.sendStatus(404)
         }
+
+        updates.forEach(update => task[update] = req.body[update])
+        await task.save()
+
         res.send(task)
     } catch (e) {
         console.log(e)
@@ -66,9 +69,9 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if (!task) {
             return res.sendStatus(404)
         }
