8a77e7afc03c2c5bbc3ef5dcaacd06f25e086d30
Dan McElroy <daniel@mcelroys.net>
2021-01-29 13:17:55 +0100

    Added password encryption, and reworked patch endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -49,7 +49,12 @@ router.patch('/users/:id', async (req, res) => {
         return res.status(400).send({ error: `Invalid updates! Field(s) ${disallowedUpdates.join(', ')} cannot be updated.` })
     }
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        updates.forEach(update => user[update] = req.body[update])
+
+        await user.save()
+
         if (!user) {
             return res.sendStatus(404)
         }
