045c0bbae4497a85e0439ca0711a999cfa82fba7
Dan McElroy <daniel@mcelroys.net>
2021-01-30 13:10:20 +0100

    Removed playground folder.

--- playground/promise-chaining-2.js
+++
@@ -1,9 +0,0 @@
-require('../src/db/mongoose')
-const Task = require('../src/models/task')
-
-const deleteTaskAndCount = async (id) => {
-    await Task.findByIdAndDelete(id)
-    return await Task.countDocuments({ completed: false })
-}
-
-deleteTaskAndCount('6012d29d3d7e9a25c06c8d7c').then(console.log).catch(console.log)
--- playground/promise-chaining.js
+++
@@ -1,9 +0,0 @@
-require('../src/db/mongoose')
-const User = require('../src/models/user')
-
-const updateAgeAndCount = async (id, age) => {
-    await User.findByIdAndUpdate(id, { age: age })
-    return await User.countDocuments({ age })
-}
-
-updateAgeAndCount('6012baef815be837c81c3a3c', 3).then(console.log).catch(console.log)
