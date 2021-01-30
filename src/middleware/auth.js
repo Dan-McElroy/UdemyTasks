550e3363376e54734c60514a11ea43ed705b4958
Dan McElroy <daniel@mcelroys.net>
2021-01-30 14:59:35 +0100

    Added authenticated tests

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,7 +13,6 @@ const auth = async (req, res, next) => {
         req.token = token
         next()
     } catch (e) {
-        console.log(e)
         res.status(401).send(JSON.stringify(e))
     }
 }
