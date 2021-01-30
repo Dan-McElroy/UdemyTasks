c33334e4c8d7f42d315d9a8c8f0944a6a13f0008
Dan McElroy <daniel@mcelroys.net>
2021-01-30 16:13:54 +0100

    Added tests for patch endpoints

--- tests/__mocks__/@sendgrid/mail.js
+++ tests/__mocks__/@sendgrid/mail.js
@@ -1,8 +1,6 @@
 module.exports = {
     setApiKey() {
-
     },
     send() {
-
     }
 }
--- tests/user.test.js
+++ tests/user.test.js
@@ -80,7 +80,7 @@ test('Should not get profile for unauthenticated user', async() => {
 })
 
 test('Should delete account for user', async() => {
-    const response = await request(app).delete('/users/me')
+    await request(app).delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
@@ -94,3 +94,39 @@ test('Should not delete account for unauthenticated user', async() => {
         .expect(401)
 })
 
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    const updates = { name: 'Phyllis', email: 'drinkle@test.com', age: 28 } 
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send(updates)
+        .expect(200)
+
+    console.log(userOneId)
+    const user = await User.findById(userOneId)
+    expect(user).toEqual(expect.objectContaining(updates))
+})
+
+test('Should update valid user fields', async () => {
+    const updates = { name: 'Phyllis', _id: new mongoose.Types.ObjectId(), age: 28 } 
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send(updates)
+        .expect(400)
+
+    console.log(userOneId)
+    const user = await User.findById(userOneId)
+    expect(user).not.toEqual(expect.objectContaining(updates))
+})
